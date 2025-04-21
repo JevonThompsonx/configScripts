87bc445074836fa1b661731660695bb8236c6260
Jevon Thompson <104575457+JevonThompsonx@users.noreply.github.com>
2025-04-20 22:31:13 -0700

    Gh added and power management

--- configuration.nix
+++ configuration.nix
@@ -105,8 +105,7 @@ boot.kernelModules = [
       
       foot
       alacritty
-      github-desktop
-      nodejs
+      gh
       zoxide
       fastfetch
       eza
@@ -134,29 +133,11 @@ boot.kernelModules = [
   git
   libinput 
   tlp
+  nodejs
   ];
 
-  # Enabling tlp for power management
-  services.power-profiles-daemon.enable = false;
-  services.tlp = {
-      enable = true;
-      settings = {
-        CPU_SCALING_GOVERNOR_ON_AC = "performance";
-        CPU_SCALING_GOVERNOR_ON_BAT = "performance";
-
-        CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
-        CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
-
-        CPU_MIN_PERF_ON_AC = 0;
-        CPU_MAX_PERF_ON_AC = 100;
-        CPU_MIN_PERF_ON_BAT = 0;
-        CPU_MAX_PERF_ON_BAT = 20;
-
-       #Optional helps save long term battery health
-       START_CHARGE_THRESH_BAT0 = 40; # 40 and below it starts to charge
-       STOP_CHARGE_THRESH_BAT0 = 80; # 80 and above it stops charging
-
-      };
+  # Enabling power management
+  services.power-profiles-daemon.enable = true
 };
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
