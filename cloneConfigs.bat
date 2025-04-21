e7c2e5eaeef0555ca05f3d4c155572a9055d1fdc
Jevon Thompson <104575457+JevonThompsonx@users.noreply.github.com>
2025-04-20 22:51:17 -0700

    Create cloneConfigs.bat

---
+++ cloneConfigs.bat
@@ -0,0 +1,11 @@
+echo "Starting system config cloning"
+
+echo "Note this will only work if you've already run git auth"
+ 
+gh repo clone nvim ~/config
+gh repo clone alacritty ~/config 
+gh repo clone fish ~/config
+gh repo clone foot ~/config
+gh repo clone WPs ~/Pictures
+gh repo clone variety ~/config
+gh repo clone fastfetch ~/config
