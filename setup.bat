ad4c2dc5ffb417f264334f4e96daad79ab2209e2
Jevon Thompson <104575457+JevonThompsonx@users.noreply.github.com>
2025-05-11 03:20:39 +0000

    Delete setup.bat

--- setup.bat
+++
@@ -1,7 +0,0 @@
-echo "Let's do some first time setup"
-chmod +x /etc/nixos/cloneConfigs.bat
-echo "Cloning github configs"
-gh auth
-/etf/nixos/cloneConfigs.bat
-sudo tailscale up
-echo "please connect to nextcloud via tailscale ip"
