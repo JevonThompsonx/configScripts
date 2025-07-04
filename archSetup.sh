eebf3edbe30d9aaac2cfee8df06e6af7153c1b04
Jevon Thompson <104575457+JevonThompsonx@users.noreply.github.com>
2025-07-04 07:49:23 +0000

    Update archSetup.sh w/ hyprlock n idle

--- archSetup.sh
+++ archSetup.sh
@@ -2,7 +2,7 @@
 
 echo "Updating system and installing base tools..."
 sudo pacman -Syu 
-sudo pacman -S git curl neovim nodejs npm fish fzf cargo zoxide unzip ripgrep npm nodejs python-pip nextcloud-client zoxide python-virtualenv gcc base-devel wget zoxide fastfetch alacritty foot librewolf-bin vivaldi eza obsidian localsend freetube-bin tailscale lazygit selene-bin webapp-manager ttf-fira-code ttf-firacode-nerd freedownloadmanager
+sudo pacman -S git curl neovim nodejs npm fish fzf cargo zoxide unzip ripgrep npm nodejs python-pip hyprlock hypridle nextcloud-client zoxide python-virtualenv gcc base-devel wget zoxide fastfetch alacritty foot librewolf-bin vivaldi eza obsidian localsend freetube-bin tailscale lazygit selene-bin webapp-manager ttf-fira-code ttf-firacode-nerd freedownloadmanager
 
 echo "creating project folder" 
 
